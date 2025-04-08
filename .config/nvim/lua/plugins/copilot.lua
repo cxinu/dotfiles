6160365ec88fee9483dce96f2b82b73154bc13ac
cxinu <72374438+cxinu@users.noreply.github.com>
2025-04-09 04:07:12 +0530

    improve remaps, rm mini plugin

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,29 +1,27 @@
 local copilot = {
     "github/copilot.vim",
-    -- event = "InsertEnter",
-    cmd = 'Copilot',
+    event = "InsertEnter",
+    ft = { "markdown", "text" },
     config = function()
         vim.g.copilot_no_tab_map = true
         vim.keymap.set("i", "<C-a>", 'copilot#Accept("<CR>")',
             { noremap = true, silent = true, expr = true, replace_keycodes = false })
-    end,
-}
+        vim.cmd("Copilot disable")
 
--- TODO: connect copilot-cmp with blink.cmp
-local copilot_cmp = {
-    "zbirenbaum/copilot-cmp",
-    event = "InsertEnter",
-    config = function() require("copilot_cmp").setup() end,
-    dependencies = {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        config = function()
-            require("copilot").setup({
-                suggestion = { enabled = false },
-                panel = { enabled = false },
-            })
-        end,
-    },
+        -- Global flag and toggle keymap
+        vim.g.copilot_enabled = false
+        vim.keymap.set('n', '<leader>tc', function()
+            if vim.g.copilot_enabled then
+                vim.cmd("Copilot disable")
+                vim.g.copilot_enabled = false
+                print("Copilot disabled")
+            else
+                vim.cmd("Copilot enable")
+                vim.g.copilot_enabled = true
+                print("Copilot enabled")
+            end
+        end, { desc = "Toggle Copilot", noremap = true, silent = true })
+    end,
 }
 
 return { copilot }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,12 +3,8 @@ return {
     branch = "0.1.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        {
-            "nvim-telescope/telescope-fzf-native.nvim",
-            build = "make",
-            cond = function() return vim.fn.executable("make") == 1 end,
-        },
-        { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+        { "nvim-tree/nvim-web-devicons",              enabled = vim.g.have_nerd_font },
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     config = function()
         local action_layout = require("telescope.actions.layout")
@@ -26,31 +22,26 @@ return {
                 man_pages = {
                     sections = { "1", "2", "3", "3p", "4", "5", "6", "7", "8", "9", "n", "l" }
                 },
-                live_grep = {
-                    grep_open_files = true,
-                    prompt_title = "Live Grep in Open Files",
-                }
             },
         })
 
-        pcall(require("telescope").load_extension, "fzf")
+        require("telescope").load_extension("fzf")
 
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-        vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-        vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-        vim.keymap.set("n", "<leader>sb", builtin.builtin, { desc = "[S]earch [B]uiltins" })
-        vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-        vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-        vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-        vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-        vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-        vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-        vim.keymap.set("n", "<leader>st", vim.cmd.TodoTelescope, { desc = "[S]earch [T]odos" })
-        vim.keymap.set("n", "<leader>s/", builtin.live_grep, { desc = "[S]earch [/] in Open Files" })
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
+        vim.keymap.set("n", "<leader>fn", function() builtin.find_files({ cwd = vim.fn.stdpath("config") }) end,
+            { desc = "[F]ind [N]eovim files" })
+        vim.keymap.set("n", "<leader>fm", builtin.man_pages, { desc = "[F]ind [M]an Pages" })
+        vim.keymap.set("n", "<leader>fb", builtin.builtin, { desc = "[F]ind [B]uiltins" })
+        vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymaps" })
+        vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
+        vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
+        vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
+        vim.keymap.set("n", "<leader>ft", vim.cmd.TodoTelescope, { desc = "[F]ind [T]odos" })
+        vim.keymap.set("n", "<leader>f.", builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
+        vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Existing buffers" })
         vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = "[/] Fuzzily search buffer" })
-        vim.keymap.set("n", "<leader>sn", function() builtin.find_files({ cwd = vim.fn.stdpath("config") }) end,
-            { desc = "[S]earch [N]eovim files" })
-        vim.keymap.set("n", "<leader>sm", builtin.man_pages, { desc = "[S]earch [M]an Pages" })
     end,
 }
