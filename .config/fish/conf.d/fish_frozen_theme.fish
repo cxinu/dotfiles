04147c0c4e3109ead2b6091b1d1a1ad06a2cc239
cxinu <72374438+cxinu@users.noreply.github.com>
2026-01-12 21:48:58 +0530

    fish & hyprland updates
    new fish doesn't use global variable defauls for key_binding & theme,
    and has been `frozen_` moved into these files.
    similarly hypr doesn't recognize 'blur' in layerrules, removed because
    it was redundant anyway.

---
+++ .config/fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,48 @@
+# This file was created by fish when upgrading to version 4.3, to migrate
+# theme variables from universal to global scope.
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+# See also the release notes for fish 4.3.0 (run `help relnotes`).
+
+set --global fish_color_autosuggestion 5c5c70
+set --global fish_color_cancel 8B6D76 --reverse
+set --global fish_color_command 817E80
+set --global fish_color_comment 5c5c70
+set --global fish_color_cwd 817E80
+set --global fish_color_cwd_root 8B6D76
+set --global fish_color_end 937A86
+set --global fish_color_error 8B6D76
+set --global fish_color_escape A49AA0
+set --global fish_color_history_current --bold
+set --global fish_color_host CB6E8B
+set --global fish_color_host_remote CB6E8B
+set --global fish_color_keyword A49AA0
+set --global fish_color_match --background=CB6E8B
+set --global fish_color_normal c2c2c6
+set --global fish_color_operator CCA9B3
+set --global fish_color_option 937A86
+set --global fish_color_param CB6E8B
+set --global fish_color_quote 937A86
+set --global fish_color_redirection A49AA0
+set --global fish_color_search_match --background=5c5c70
+set --global fish_color_selection --background=5c5c70
+set --global fish_color_status 8B6D76
+set --global fish_color_user 817E80
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_background 0c0c1d
+set --global fish_pager_color_completion c2c2c6
+set --global fish_pager_color_description 5c5c70
+set --global fish_pager_color_prefix 817E80
+set --global fish_pager_color_progress 5c5c70
+set --global fish_pager_color_secondary_background 0c0c1d
+set --global fish_pager_color_secondary_completion c2c2c6
+set --global fish_pager_color_secondary_description 5c5c70
+set --global fish_pager_color_secondary_prefix 817E80
+set --global fish_pager_color_selected_background --background=5c5c70
+set --global fish_pager_color_selected_completion c2c2c6
+set --global fish_pager_color_selected_description 5c5c70
+set --global fish_pager_color_selected_prefix 817E80
